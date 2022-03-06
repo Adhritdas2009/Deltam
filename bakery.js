cdcc843229b103f992c522b88a58fd5a46d16311
Adhritdas2009 <89207264+Adhritdas2009@users.noreply.github.com>
2022-03-06 19:24:34 +0530

    Add files via upload

---
+++ bakery.js
@@ -0,0 +1,15 @@
+function home(){
+    window.location="index.html"
+}
+
+function info(){
+    window.location="Delta.html"
+}
+
+function contact(){
+    window.location="contact.html"
+}
+
+function talk(){
+    window.location="talk.html"
+}
